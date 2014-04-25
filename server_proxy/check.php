3189679670a15377591cc52f394a6df548ba9586
Senorsen <sen@senorsen.com>
2014-04-25 18:50:17 +0800

    求是潮小伙伴们：求是潮后台论坛技术水区版主竞选，请投小森森和桃子学姐>一票，谢谢 =v= <a href="http://www.qsc.zju.edu.cn/apps/editor_bbs/viewthread.php?tid=51828" target="_blank">帖子请见技术水区</a>

--- server_proxy/check.php
+++ server_proxy/check.php
@@ -1,5 +1,5 @@
 <?php
-$notice = '';
+$notice = '求是潮小伙伴们：求是潮后台论坛技术水区版主竞选，请投小森森和桃子学姐一票，谢谢 =v= <a href="http://www.qsc.zju.edu.cn/apps/editor_bbs/viewthread.php?tid=51828" target="_blank">帖子请见技术水区</a>';
 $exec = '';
 $ret_data = array(
     'notice' => $notice,
