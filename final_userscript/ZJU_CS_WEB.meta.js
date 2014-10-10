24421f13c090a98cfcc2586afabb2ab9b81487ed
Senorsen <sen@senorsen.com>
2014-10-11 00:29:22 +0800

    use my own vps to provide metadata.

---
+++ final_userscript/ZJU_CS_WEB.meta.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name       ZJU-CS-WEB
+// @namespace  http://www.senorsen.com
+// @version    0.3.6-beta
+// @description  Make ZJU-Computer science basic course webpage more beautiful & compatible in mordern browsers.
+// @match      http://blackwhite.8866.org/cstcx/web/index.asp?*
+// @match      http://10.71.45.100/cstcx/web/index.asp?*
+// @match      http://blackwhite.8866.org/
+// @match      http://10.71.45.100/
+// @match      http://10.77.30.31/
+// @match      http://10.77.30.33/
+// @match      http://10.77.30.30/
+// @updateURL   https://prj.senorsen.com/zju-csweb/final_userscript/ZJU_CS_WEB.meta.js
+// @downloadURL https://prj.senorsen.com/zju-csweb/final_userscript/ZJU_CS_WEB.user.js
+// @copyright  2013-2014, Senorsen
+// @run-at document-body
+// ==/UserScript==
+
--- final_userscript/ZJU_CS_WEB.user.js
+++ final_userscript/ZJU_CS_WEB.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       ZJU-CS-WEB
 // @namespace  http://www.senorsen.com
-// @version    0.3.5beta2
+// @version    0.3.6-beta
 // @description  Make ZJU-Computer science basic course webpage more beautiful & compatible in mordern browsers.
 // @match      http://blackwhite.8866.org/cstcx/web/index.asp?*
 // @match      http://10.71.45.100/cstcx/web/index.asp?*
@@ -10,8 +10,8 @@
 // @match      http://10.77.30.31/
 // @match      http://10.77.30.33/
 // @match      http://10.77.30.30/
-// @updateURL   http://app.myqsc.com/zju-csweb/final_userscript/ZJU_CS_WEB.meta.js
-// @downloadURL http://app.myqsc.com/zju-csweb/final_userscript/ZJU_CS_WEB.user.js
+// @updateURL   https://prj.senorsen.com/zju-csweb/final_userscript/ZJU_CS_WEB.meta.js
+// @downloadURL https://prj.senorsen.com/zju-csweb/final_userscript/ZJU_CS_WEB.user.js
 // @copyright  2013-2014, Senorsen
 // @run-at document-body
 // ==/UserScript==
